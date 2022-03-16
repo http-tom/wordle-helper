aac7d149d1e0988b992437f4ea9da802b53b02f0
Tom <tom@freedom-web.co.uk>
2022-03-16 20:56:09 +0000

    Easier to use interface

--- public_html/app.js
+++ public_html/app.js
@@ -1,6 +1,4 @@
 (function() {
-    let btnExclude = document.getElementById('addExclude');
-    let btnInclude = document.getElementById('addInclude');
     let frm = document.getElementById('frmAjaxHelper');
     let toggles = document.getElementsByClassName('toggler');
 
@@ -20,27 +18,86 @@
     }
 
 
-    btnExclude.addEventListener('click', function() {
-        let input = document.createElement('input');
-        input.type="text";
-        input.name="exclude[]";
-        input.maxLength = 4;
-        input.size = 5;
-        input.classList = 'frm-control';
-        input.autocomplete = 'off';
-        this.parentNode.insertBefore(input, this);
-    });
 
-    btnInclude.addEventListener('click', function() {
-        let input = document.createElement('input');
-        input.type="text";
-        input.name="include[]";
-        input.maxLength = 4;
-        input.size = 5;
-        input.classList = 'frm-control';
-        input.autocomplete = 'off';
-        this.parentNode.insertBefore(input, this);
-    });
+
+
+
+
+    function evtCellKeyUp(e, el, cellObj) {
+        if(el.value.length == 0) {
+            if(document.getElementsByClassName(cellObj.className).length == 1) {
+                return;
+            }
+            el.previousElementSibling.focus();
+            el.remove();
+        } else {
+            if(el.nextElementSibling === 'undefined' || el.nextElementSibling.classList.contains(cellObj.className) === false) {
+                let input = document.createElement('input');
+                input.type="text";
+                input.name=cellObj.name+"[]";
+                input.maxLength = 1;
+                input.size = 1;
+                input.classList = 'frm-control '+cellObj.className;
+                input.autocomplete = 'off';
+                input.addEventListener('keyup', function(e) {
+                    evtCellKeyUp(e,this,cellObj);
+                });
+                el.parentNode.insertBefore(input, el.nextElementSibling);
+                input.focus();
+            }
+        }
+    }
+
+
+    let charCells = [
+        {
+            className: 'includeChar',
+            name: 'include'
+        },
+        {
+            className: 'excludeChar',
+            name: 'exclude'
+        }
+    ];
+
+    for(let i = 0; i < charCells.length; i++) {
+        let incCharEls = document.getElementsByClassName(charCells[i].className);
+        for(let j = 0; j < incCharEls.length; j++) {
+            let element = incCharEls[j];
+            element.addEventListener('keyup', function(e) {
+                evtCellKeyUp(e,element,charCells[i]);
+            });
+        }
+    }
+
+
+
+    let cellTemplates = document.getElementsByClassName('charTemplate');
+    for(let i = 0; i < cellTemplates.length; i++) {
+        cellTemplates[i].addEventListener('keydown', function(e) {
+            // overwrite contents if new letter input
+            if(this.value.length == 1) {
+                this.value = '';
+            }
+            if(false===((e.keyCode > 64 && e.keyCode < 91) || e.keyCode == 8)) {
+                e.preventDefault();
+                return false;
+            }
+        });
+        cellTemplates[i].addEventListener('keyup', function(e) {
+            if((e.keyCode > 64 && e.keyCode < 91)  || e.keyCode == 9 || e.keyCode == 13 || e.keyCode == 39) {
+                // A-Z, backspace, enter, right
+                if(this.nextElementSibling !== null) {
+                    this.nextElementSibling.focus();
+                }
+            }
+            if(e.keyCode == 8 || e.keyCode == 37 || (e.keyCode == 9 && e.shiftKey === true)) {
+                // backspace, left
+                this.previousElementSibling.focus();
+            }
+        });
+    }
+
 
     frm.addEventListener('submit', function(e) {
         e.preventDefault();
--- public_html/index.php
+++ public_html/index.php
@@ -8,34 +8,40 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>WordleHelper</h1>
-    <button role="button" class="btn rounded toggler" data-toggle="help-description"><h3 class="m0 p0">?</h3></button>
-    <div id="help-description" class="hidden">
-        <h4>A Wordle helper.</h4>
-        <p>Enter no characters into the boxes to get a random suggestion (starting word).<br/>Use the boxes to enter a character or group of characters. Use the 'Easy Mode' to specify where the characters are placed in the word.</p>
+    <header>
+        <strong></strong>
+        <strong></strong>
+        <button role="button" class="btn btn-util toggler" data-toggle="help-description"><h3 class="m0 p0">?</h3></button>
+    </header>
+    <div class="container">
+        <h1>WordleHelper</h1>
+        <div id="help-description" class="hidden">
+            <h4>A Wordle helper.</h4>
+            <p>Enter no characters into the boxes to get a random suggestion (starting word).<br/>Use the boxes to enter a character or group of characters. Use the 'Easy Mode' to specify where the characters are placed in the word.</p>
+        </div>
+        <form method="post" action="wordle.php" id="frmAjaxHelper">
+            <h4>Include Characters</h4>
+            <input type="text" name="include[]" maxlength="1" size="1" class="frm-control includeChar" autocomplete="off">
+            <h4>Exclude Characters</h4>
+            <input type="text" name="exclude[]" maxlength="1" size="1" class="frm-control excludeChar" autocomplete="off">
+
+            <div class="mt-1">
+                <h4>Easy Mode</h4>
+                <input type="text" name="template_1" maxlength="1" size="1" class="frm-control charTemplate" autocomplete="off">
+                <input type="text" name="template_2" maxlength="1" size="1" class="frm-control charTemplate" autocomplete="off">
+                <input type="text" name="template_3" maxlength="1" size="1" class="frm-control charTemplate" autocomplete="off">
+                <input type="text" name="template_4" maxlength="1" size="1" class="frm-control charTemplate" autocomplete="off">
+                <input type="text" name="template_5" maxlength="1" size="1" class="frm-control charTemplate" autocomplete="off">
+            </div>
+
+            <div>
+                <input type="submit" value="Go" class="btn m1">
+            </div>
+
+            <div id="results"></div>
+            <div id="suggestions"></div>
+        </form>
     </div>
-    <form method="post" action="wordle.php" id="frmAjaxHelper">
-        <h4>Include Characters</h4>
-        <input type="text" name="include[]" maxlength="4" size="5" class="frm-control" autocomplete="off"> <button type="button" id="addInclude" class="btn">+</button>
-        <h4>Exclude Characters</h4>
-        <input type="text" name="exclude[]" maxlength="24" size="5" class="frm-control" autocomplete="off"> <button type="button" id="addExclude" class="btn">+</button>
-
-        <div class="mt-1">
-            <h4>Easy Mode</h4>
-            <input type="text" name="template_1" maxlength="1" size="1" class="frm-control" autocomplete="off">
-            <input type="text" name="template_2" maxlength="1" size="1" class="frm-control" autocomplete="off">
-            <input type="text" name="template_3" maxlength="1" size="1" class="frm-control" autocomplete="off">
-            <input type="text" name="template_4" maxlength="1" size="1" class="frm-control" autocomplete="off">
-            <input type="text" name="template_5" maxlength="1" size="1" class="frm-control" autocomplete="off">
-        </div>
-
-        <div>
-            <input type="submit" value="Go" class="btn m1">
-        </div>
-
-        <div id="results"></div>
-        <div id="suggestions"></div>
-    </form>
 
     <footer>
         View source code on <a href="https://github.com/http-tom/wordle-helper" target="_blank">GitHub</a>
